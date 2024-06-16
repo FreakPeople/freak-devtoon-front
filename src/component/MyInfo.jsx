4e9bc862230c2359461e9c606ff2aece784eadeb
jeongjihun <wlgns2234@naver.com>
2024-06-16 13:18:55 +0900

    feat: my page UI 구현

--- src/component/MyInfo.jsx
+++ src/component/MyInfo.jsx
@@ -1,16 +1,90 @@
+import EmailIcon from '@mui/icons-material/Email';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import WarningIcon from '@mui/icons-material/Warning';
+import CookieIcon from '@mui/icons-material/Cookie';
+
 
 function MyInfo(props) {
 
     const myInfoStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '4px 4px 4px 4px',
         border: '1px solid black'
     }
 
-    const {myInfo} = props;
+    const firstLine = {
+        display: 'flex',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        marginBottom: '20px'
+    }
+
+    const name = {
+        padding: '3px 6px 4px 6px',
+        marginRight: '8px',
+        backgroundColor: '#4f4f4f',
+        height: '18px',
+        fontSize: '18px',
+        color: 'white',
+        border: 'solid 1px',
+        borderRadius: '6px'
+    }
+
+    const membershipStatus = {
+        marginTop: '2px',
+        marginRight: '2px',
+        padding: '3px 6px 4px 6px',
+        height: '16px',
+        fontSize: '16px',
+        color: '#615F5C',
+        border: 'solid 1px #615F5C',
+        borderRadius: '6px'
+    }
+
+    const lineFont = {
+        margin: '2px',
+        height: '14px',
+        fontSize: '14px',
+        color: '#615F5C'
+    }
+
+    const infoLine = {
+        display: 'flex',
+        alignItems: 'center',
+        marginBottom: '3px'
+    }
+
+    const infoIcon = {
+        color: '#615F5C'
+    }
+
+    const {myInfo, myWarningCount, myCookieQuantity} = props;
 
     return (
         <div style={myInfoStyle}>
-            <p>{myInfo['name']}</p>
-            <p>{myInfo['membershipStatus']}</p>
+            <div style={firstLine}>
+                <div style={name}>{myInfo['name']}</div>
+                <div style={membershipStatus}>{myInfo['membershipStatus']}</div>
+            </div>
+            <div style={infoLine}>
+                <EmailIcon style={infoIcon} sx={{ fontSize: 12 }}></EmailIcon>
+                <div style={lineFont}>이메일 : {myInfo['email']}</div>
+            </div>
+            <div style={infoLine}>
+                <CalendarMonthIcon style={infoIcon} sx={{ fontSize: 12}}></CalendarMonthIcon>
+                {/*이 부분만 substring 사용하면 에러 발생*/}
+                <div style={lineFont}>가입일 : {myInfo['createdAt']}</div>
+            </div>
+            <div style={infoLine}>
+                <WarningIcon style={infoIcon} sx={{ fontSize: 12}}></WarningIcon>
+                <div style={lineFont}>비속어 경고 횟수 : {myWarningCount['count']}</div>
+            </div>
+            <div style={infoLine}>
+                <CookieIcon style={infoIcon} sx={{ fontSize: 12}}></CookieIcon>
+                <div style={lineFont}>보유 쿠키 개수 : {myCookieQuantity['quantity']}</div>
+            </div>
+
         </div>
     )
 }
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -3,14 +3,30 @@ import axios from "axios";
 import MyInfo from "../component/MyInfo.jsx";
 
 function MyPage() {
-    const bodyStyle = {
-        border: '1px solid black'
+    const bodyContainer = {
+        display: 'flex',
     };
 
+    const sideBar = {
+        flex: '1',
+    }
+
+    const myInfoContent = {
+        marginTop: '50px',
+        flex: '4',
+    }
+
+    const myInfoTitle = {
+        marginBottom: '20px'
+    }
+
     const [myInfo, setMyInfo] = useState([])
+    const [myWarningCount, setMyWarningCount] = useState([])
+    const [myCookieQuantity, setMyCookieQuantity] = useState([])
+
+    const memberId = 1
 
     useEffect(() => {
-        let memberId = 1
         axios({
             method: 'GET',
             url: 'http://localhost:8080/v1/webtoon-viewers/' + memberId
@@ -19,10 +35,37 @@ function MyPage() {
         })
     }, []);
 
+    useEffect(() => {
+        axios({
+            method: 'GET',
+            url: 'http://localhost:8080/v1/bad-words-warning-count?webtoonViewerNo=' + memberId
+        }).then((response) => {
+            setMyWarningCount(response.data.data)
+        })
+    }, []);
+
+    useEffect(() => {
+        axios({
+            method: 'GET',
+            url: 'http://localhost:8080/v1/cookie-wallets?webtoonViewerNo=' + memberId
+        }).then((response) => {
+            setMyCookieQuantity(response.data.data)
+        })
+    }, []);
+
     return (
-        <div style={bodyStyle}>
-            <div>마이 페이지</div>
-            <MyInfo myInfo={myInfo}/>
+        <div style={bodyContainer}>
+            <div style={sideBar}></div>
+            <div style={myInfoContent}>
+                <div style={myInfoTitle}>마이 페이지</div>
+                <MyInfo
+                    myInfo={myInfo}
+                    myWarningCount={myWarningCount}
+                    myCookieQuantity={myCookieQuantity}
+                />
+            </div>
+            <div style={sideBar}></div>
+
         </div>
     );
 }
